177b92137daca6c1b3196e35a46fc6b27ac49746
VitorMoreiraAraujo <vitormoreira.araujo@gmail.com>
2023-05-30 22:03:55 -0300

    Ajeitando o spinner

--- app/src/main/java/br/com/etecia/appbuscacep_api/MainActivity.java
+++ app/src/main/java/br/com/etecia/appbuscacep_api/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,12 +18,16 @@ public class MainActivity extends AppCompatActivity {
     Button btnBuscarCep;
     EditText txtCep, lblCEP, lblLogradouro, lblComplemento, lblBairro, lblCidade;
 
+    String [] item = {"Estados"};
+
     Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         txtCep = findViewById(R.id.txtCep);
         lblCEP = findViewById(R.id.lblCEP);
@@ -32,11 +37,26 @@ public class MainActivity extends AppCompatActivity {
         lblCidade = findViewById(R.id.lblCidade);
 
         btnBuscarCep = findViewById(R.id.btnBuscaCep);
-        //spinner = findViewById(R.id.spinnerEstado);
+        spinner = findViewById(R.id.spinnerEstado);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, item);
+        spinner.setAdapter(adapter);
 
         //String [] estado = getResources().getStringArray(R.array.estados_array);
 
-        //spinner.setAdapter( new ArrayAdapter<String>(getApplicationContext(), R.layout.activity_main, estado));
+        //spinner.setAdapter( new ArrayAdapter<String>(getApplicationContext(), R.layout.activity_main, estado));.
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item[0] = spinner.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         btnBuscarCep.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
                     lblComplemento.setText(retorno.getComplemento().toString());
                     lblBairro.setText(retorno.getBairro().toString());
                     lblCidade.setText(retorno.getLocalidade().toString());
+                    item [0] = retorno.getUf().toString();
                     //spinner.setText(retorno.getUf().toString());
 
 
